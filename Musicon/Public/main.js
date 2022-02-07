6ac9c2bf638f9ce4cf18884b5a1ebcfc7c5fb45d
Hyeo8 <hyeo8@wisc.edu>
2022-02-06 23:03:08 -0600

    18-38

--- Musicon/Public/main.js
+++ Musicon/Public/main.js
@@ -30,4 +30,13 @@ const context = {
       }
     ]
   };
-  
+
+const templateElement = document.getElementById("templateHB")
+
+const templateSource = templateElement.innerHTML;
+
+const template = Handlebars.compile(templateSource);
+
+const compiledHtml = template(context);
+
+document.getElementById("information").innerHTML = compiledHtml;
