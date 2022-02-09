c36afb5e1fea10a1ef32009d60f83efab4a23e5f
Hyeo8 <hyeo8@wisc.edu>
2022-02-08 21:03:28 -0600

    init RockPaperScissors Project

---
+++ Rock_Paper_Scissors/js/game-logic.js
@@ -0,0 +1,25 @@
+// All code should be written in this file.
+let playerOneMoveOneType,
+    playerOneMoveTwoType,
+    playerOneMoveThreeType,
+    playerTwoMoveOneType,
+    playerTwoMoveTwoType,
+    playerTwoMoveThreeType,
+    playerOneMoveOneValue,
+    playerOneMoveTwoValue,
+    playerOneMoveThreeValue,
+    playerTwoMoveOneValue,
+    playerTwoMoveTwoValue,
+    playerTwoMoveThreeValue;
+
+/* create `setPlayerMoves()` and pass in parameters `player, moveOneType, moreOneValue, moveTwoType, 
+moveThreeType, moreThreeValue */
+
+let setPlayerMoves = (player, moveOneType, moreOneValue, moveTwoType, moveThreeType, moreThreeValue) => {
+    //create failfast return if any of the values were not provided
+    if(!moveOneType || !moreOneValue || !moveTwoType || !moveThreeType || !moreThreeValue) {
+        return;
+    }
+
+
+}
