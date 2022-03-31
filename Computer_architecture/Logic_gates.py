7a2dc4340193ae9c0c0afd29c70f28c206518c96
Hyeo8 <hyeo8@wisc.edu>
2022-03-31 14:11:59 -0500

    logic gates

---
+++ Computer_architecture/Logic_gates.py
@@ -0,0 +1,12 @@
+# Not Gate
+# returns 1 only when it is 0
+
+# And gate
+# returns 1 when both are 1
+
+# Or gate
+# returns 1 when either or both are 1
+
+# NOR Gate = Not + Or 
+
+# NAND Gate = Not + And
