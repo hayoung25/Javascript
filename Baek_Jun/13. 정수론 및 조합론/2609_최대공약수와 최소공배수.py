ddb1122eeca5db212ea5a7ba5db714bc3d94bc0c
Hyeo8 <hyeo8@wisc.edu>
2022-04-21 08:26:32 -0500

    1934

--- Baek_Jun/13. 정수론 및 조합론/2609_최대공약수와 최소공배수.py
+++ Baek_Jun/13. 정수론 및 조합론/2609_최대공약수와 최소공배수.py
@@ -7,20 +7,19 @@
 # 출력
 # 첫째 줄에는 입력으로 주어진 두 수의 최대공약수를, 둘째 줄에는 입력으로 주어진 두 수의 최소 공배수를 출력한다.
 
-import sys
-input = sys.stdin.readline
-
 a, b = map(int, input().split())
 
-for i in reversed(range(2, min(a + 1, b + 1))):
-    if a % i == 0 and b % i ==0:
-        print(i)
-        break
-    
-for i in range(max(a,b), a ** b):
-    if i % a == 0 and i % b ==0:
-        print(i)
-        break
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+print(gcd(a, b))
+print(lcm(a, b))
 
 # 예제 입력 1 
 # 24 18
